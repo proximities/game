51661e0cc806b4046b02b5fd54550dde5857273d
Gan <billgan2@amd.com>
2024-01-16 19:38:48 -0500

    hi

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/billgan2/code/game
+# Build directory: C:/Users/billgan2/code/game/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
